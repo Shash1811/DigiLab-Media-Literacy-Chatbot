0402813fc70c47e7a9754f020e46c5170bacc433
SagarHedav <sagarhedav@gmail.com>
2026-02-19 23:42:59 +0530

    feat: integrate frontend chat with deployed HuggingFace chatbot API

--- crypt/src/lib/chatbotApi.js
+++ crypt/src/lib/chatbotApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const chatbotClient = axios.create({
-    baseURL: 'http://localhost:8000',
+    baseURL: 'https://asvix-digilab.hf.space',
     headers: {
         'Content-Type': 'application/json',
     },
